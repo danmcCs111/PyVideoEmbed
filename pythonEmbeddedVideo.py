036f0d859ee6912c2964368fb908829b7791a426
danmcCs111 <danmccs111@gmail.com>
2026-07-13 16:18:54 -0400

    add resize as optional params

--- pythonEmbeddedVideo.py
+++ pythonEmbeddedVideo.py
@@ -4,12 +4,18 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 
 videoLink = sys.argv[1]
+if(sys.argv.__len__() > 3):
+    videoWidth = int(sys.argv[2])
+    videoHeight = int(sys.argv[3])
+else:
+    videoWidth = 800
+    videoHeight = 450
 
 # Basic PyQt6 window setup
 app = QApplication(sys.argv)
 window = QMainWindow()
 window.setWindowTitle("PyQt6 Video Embed")
-window.resize(800, 450)
+window.resize(videoWidth, videoHeight)
 
 # Initialize and configure QWebEngineView
 browser = QWebEngineView()
