b05899232592b95d165530c19a1d4ad3daa0bf6c
danmcCs111 <danmccs111@gmail.com>
2026-07-13 16:32:37 -0400

    add fullscreen option

--- pythonEmbeddedVideo.py
+++ pythonEmbeddedVideo.py
@@ -3,19 +3,38 @@ from PyQt6.QtCore import QUrl
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 
-videoLink = sys.argv[1]
-if(sys.argv.__len__() > 3):
-    videoWidth = int(sys.argv[2])
-    videoHeight = int(sys.argv[3])
+videoWidth = 800
+videoHeight = 450
+
+isFull = False
+
+argIt = 1
+
+if("-f" == sys.argv[argIt] or "--fullscreen" == sys.argv[argIt]):
+        isFull = True
+        argIt += 1
+        videoLink = sys.argv[argIt]
+        argIt += 1
 else:
-    videoWidth = 800
-    videoHeight = 450
+        videoLink = sys.argv[argIt]
+        argIt += 1
+
+if(sys.argv.__len__() > argIt+1):
+    videoWidth = int(sys.argv[argIt])
+    argIt += 1
+    videoHeight = int(sys.argv[argIt])
+    argIt += 1
 
 # Basic PyQt6 window setup
 app = QApplication(sys.argv)
 window = QMainWindow()
+
+if(isFull):
+    window.showFullScreen()
+else:
+    window.resize(videoWidth, videoHeight)
 window.setWindowTitle("PyQt6 Video Embed")
-window.resize(videoWidth, videoHeight)
+
 
 # Initialize and configure QWebEngineView
 browser = QWebEngineView()
